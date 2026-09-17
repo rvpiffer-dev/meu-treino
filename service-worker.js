fdc62f5005e6770173e3a259c6c26720a96e8a53
rvpiffer-dev <rvpiffer@gmail.com>
2026-09-17 17:20:01 -0300

    Corrige atualização e cache da PWA

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='meu-treino-v1';const ASSETS=['./','./index.html','./css/app.css','./js/app.js','./js/workout.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='meu-treino-v4';
+const ASSETS=['./','./index.html','./css/app.css','./js/app.js','./js/workout.js','./manifest.json'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});
